56889c922d5ad4f347d33b790d5f38d0c52c5b3f
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-07-05 11:43:26 +0000

    style: card in progress teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-// import { Card } from "@/components/ui/card";
+
+import CardPage from "@/pages/Card/Card";
 // import CardPage from "@/pages/Card/Card";
 import Header from "@/pages/Header/Header";
 
 export default function Home() {
   return (
     <> 
-      <Header />
-      {/* <CardPage /> */}
+      {/* <Header /> */}
+       <CardPage /> 
     
     </>
   );
--- src/pages/Card/Card.tsx
+++ src/pages/Card/Card.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Card,
   CardContent,
@@ -6,22 +7,44 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useState } from "react";
 
-export default function CardPage() {
+
+export default function CardPage(){
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+ 
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(name, email);
+  } 
+  
+
+
+
+  
+  
+
   return (
-    <Card className="bg-slate-500 w-96 shadow-md shadow-black">
+
+    <Card className="bg-slate-100 w-96 shadow-md shadow-black">
       <CardHeader>
-        <CardTitle>Cadastro do Bolsa Escola</CardTitle>
-        <CardDescription>Bolsa escola Brasil, um país de todes! </CardDescription>
+        <CardTitle >Cadastro do Bolsa Escola</CardTitle>
+        <CardDescription >Bolsa escola Brasil, um país de todes! </CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col justify-center gap-2">
-        <label>Digite seu nome: </label>
+        <label >Digite seu nome: </label>
         <input type="text" placeholder="Nome" className="border border-slate-500 rounded p-0.5"/>
-        <label>Digite seu email: </label>
-        <input type="email" placeholder="Email" className="border border-slate-500 rounded p-0.5"/>
+        <label >Digite seu email: </label>
+        <input type="email" placeholder="Email" className="border border-slate-100 rounded p-0.5"/>
       </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
+      <CardFooter className="flex justify-center items-center">
+       <div>
+         <button onClick={handleSubmit} type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+           Cadastrar
+         </button>
+       </div>
       </CardFooter>
     </Card>
 
